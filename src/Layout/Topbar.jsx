c8bcbfe560b3770563d2f94a747c4197e2fe6f8d
vacaramin <vacaramin86@gmail.com>
2025-03-31 20:35:00 +0500

    topbar animation fixes

--- src/Layout/Topbar.jsx
+++ src/Layout/Topbar.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { useState, useEffect } from "preact/hooks";
-import { Settings2 } from "lucide-preact";
+import { useState } from "preact/hooks";
+import { CircleX, Settings2 } from "lucide-preact";
 
 const Topbar = ({ className }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,19 +10,21 @@ const Topbar = ({ className }) => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {}, [apiKey]);
   const handleSave = (e) => {
     setApiKey(e.target.value);
   };
+
   return (
     <div className={className}>
-      <Settings2 onClick={handleClick}></Settings2>
-      {isOpen && (
-        <div className="inputField">
-          <input type="text" className="toggle" defaultValue={apiKey} />
-          <button onClick={handleSave}>Set Api Key</button>
-        </div>
-      )}
+      <div className="icon-wrapper" onClick={handleClick}>
+        <CircleX className={`icon ${isOpen ? "show" : ""}`} />
+        <Settings2 className={`icon ${!isOpen ? "show" : ""}`} />
+      </div>
+
+      <div className={`inputField ${isOpen && "show"}`}>
+        <input type="text" className="toggle" defaultValue={apiKey} />
+        <button onClick={handleSave}>Set Api Key</button>
+      </div>
     </div>
   );
 };
@@ -35,16 +37,67 @@ export default styled(Topbar)`
   align-items: center;
   gap: 10px;
 
+  .icon-wrapper {
+    position: relative;
+    width: 24px;
+    height: 24px;
+  }
+
+  .icon {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    opacity: 0;
+    transition:
+      opacity 0.3s ease-in-out,
+      transform 0.3s ease-in-out;
+  }
+
+  .show {
+    opacity: 1;
+    transform: scale(1);
+  }
+
   .inputField {
     display: flex;
+    align-items: center;
     gap: 5px;
-  
+    visibility: hidden;
+    transition:
+      opacity 0.3s ease-in-out,
+      transform 0.5s ease-in-out,
+      visibility 0.5s ease-in-out;
+    opacity: 0;
+    transform: translateX(50px);
+
     input {
       height: 100%;
+      padding: 5px 10px;
+      width: 30%;
+      min-width: 250px;
     }
-  
+
     button {
       height: 100%;
+      padding: 5px 10px;
+      background-color: #14a800;
+      border: none;
+      border-radius: 2px;
+      word-wrap: normal;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #2bcc16;
+      }
+    }
+
+    &.show {
+      visibility: visible;
+      opacity: 1;
+      transform: translateX(0px);
     }
   }
 `;
